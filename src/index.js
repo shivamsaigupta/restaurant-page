33c023c2938a00df39dc5c6a9a9369ddcb86f410
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2019-12-31 15:59:55 +0530

    Added linter

--- src/index.js
+++ src/index.js
@@ -1,75 +1,73 @@
-import home from './tabs/home';
-import location from './tabs/location';
-import menu from './tabs/menu';
-import about from './tabs/about';
-import resetContent from './resetContent';
-import setup from './setup';
-
+import home from "./tabs/home";
+import location from "./tabs/location";
+import menu from "./tabs/menu";
+import about from "./tabs/about";
+import resetContent from "./resetContent";
+import setup from "./setup";
 
 setup();
 home();
 
-const TabManager = (function(){
-  let lastActiveTab = 'home';
-  const homeTab = document.querySelector('#home');
-  const locationTab = document.querySelector('#location');
-  const menuTab = document.querySelector('#menu');
-  const aboutTab = document.querySelector('#about');
+const TabManager = (function() {
+  let lastActiveTab = "home";
+  const homeTab = document.querySelector("#home");
+  const locationTab = document.querySelector("#location");
+  const menuTab = document.querySelector("#menu");
+  const aboutTab = document.querySelector("#about");
   const tabs = [homeTab, locationTab, menuTab, aboutTab];
 
-  function _switchBtn(btnName){
+  function _switchBtn(btnName) {
     //if found current tab, set active, if found previous tab, remove active
     console.log(tabs);
 
     tabs.map(tab => {
-      console.log(tab, ' ', tab.id);
-      if(tab.id === btnName){
-        tab.classList.add('active');
-      }else if(tab.id === lastActiveTab){
-        tab.classList.remove('active');
+      console.log(tab, " ", tab.id);
+      if (tab.id === btnName) {
+        tab.classList.add("active");
+      } else if (tab.id === lastActiveTab) {
+        tab.classList.remove("active");
       }
     });
   }
 
-  function _changeTab(btn){
+  function _changeTab(btn) {
     resetContent();
 
     switch (btn) {
-      case 'home':
+      case "home":
         home();
         _switchBtn(btn);
-        lastActiveTab = 'home';
+        lastActiveTab = "home";
         break;
-      case 'location':
+      case "location":
         location();
         _switchBtn(btn);
-        lastActiveTab = 'location';
+        lastActiveTab = "location";
         break;
-      case 'menu':
+      case "menu":
         menu();
         _switchBtn(btn);
-        lastActiveTab = 'menu';
+        lastActiveTab = "menu";
         break;
-      case 'about':
+      case "about":
         about();
         _switchBtn(btn);
-        lastActiveTab = 'about';
+        lastActiveTab = "about";
         break;
       default:
-        throw new Error('Invalid changeTab argument.');
+        throw new Error("Invalid changeTab argument.");
     }
   }
 
-  function addEventListeners(){
+  function addEventListeners() {
     tabs.map(tab => {
-      tab.addEventListener('click', (e) => {
+      tab.addEventListener("click", e => {
         _changeTab(e.target.id);
       });
-    })
+    });
   }
 
   return { addEventListeners };
-
 })();
 
 TabManager.addEventListeners();
--- src/setup.js
+++ src/setup.js
@@ -1,20 +1,21 @@
 const setup = () => {
-  const mainContainer = document.querySelector('#main-container');
-  const header = document.createElement('header');
+  const mainContainer = document.querySelector("#main-container");
+  const header = document.createElement("header");
 
-  const titleContainer = document.createElement('div');
-  titleContainer.setAttribute('id', 'title-container');
-  const title = document.createElement('h2');
+  const titleContainer = document.createElement("div");
+  titleContainer.setAttribute("id", "title-container");
+  const title = document.createElement("h2");
   title.textContent = "Gupta's Luxurious Indian Restaurant";
   titleContainer.appendChild(title);
   header.appendChild(titleContainer);
 
-  const tabs = document.createElement('ul');
-  tabs.classList.add('tabs');
-  tabs.innerHTML = "<li id='home' class='active'>Home</li> <li id='menu'>Menu</li> <li id='location'>Location</li> <li id='about'>About</li>";
+  const tabs = document.createElement("ul");
+  tabs.classList.add("tabs");
+  tabs.innerHTML =
+    "<li id='home' class='active'>Home</li> <li id='menu'>Menu</li> <li id='location'>Location</li> <li id='about'>About</li>";
   header.appendChild(tabs);
 
   mainContainer.append(header);
-}
+};
 
 export default setup;
